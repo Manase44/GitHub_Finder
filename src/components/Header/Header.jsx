f360183aaf9fa3a5a42b9b9d91c386d6d0b36be3
Manase44 <fikopersempre@gmail.com>
2024-06-19 11:06:09 +0300

    finalized the UI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,18 +2,21 @@ import './Header.css'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
+
+  const handleSearchClick = e => {
+    e.preventDefault();
+  };
+
   return (
     <header>
-        <Link to={"/"} className="header-title">
-          <h1>github finder</h1>
-        </Link>
+        <h1 className="header-title">github finder</h1>
         <p className="header-author">By 
           <a href="" target='_blank'> Manase Gunga</a>
         </p>
         <div className="header-search">
           <form className="search-form">
-            <input type="text" name="username" id="username" placeholder='enter a username' />
-            <button>Search</button>
+            <input type="text" name="username" id="username" placeholder='enter a username'/>
+            <button onClick={handleSearchClick}>Search</button>
           </form>
         </div>
     </header>
